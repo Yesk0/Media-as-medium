4d0b96c2561f1e86d987a2c4dd22aee38896cdaf
Yesko <iskoaleksandor@gmail.com>
2025-05-13 00:07:42 +0500

    chore: deleted the script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const selector = document.getElementById('theme-selector');
-
-selector.addEventListener('change', () => {
-  document.body.className = ''; // Удаляем старую тему
-  const theme = selector.value + '-theme';
-  document.body.classList.add(theme);
-});
